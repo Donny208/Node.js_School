e42931d55914655d7884ec334290a6db2e2b2c63
Don <donvoowright@aol.com>
2016-02-02 09:05:18 -0700

    Update client.js

--- client.js
+++ client.js
@@ -1,17 +1,22 @@
 /*Code By Donovan Wright, 
 This is some client side code (the website), hopefully it should help you get started!
 */
+
+//Creating An Instance Of The WebSocket
 var ws = new WebSocket("ws://PUT_YOUR_IP_HERE:8001/");
 
+//When The Server Sends A Message
 ws.onmessage = function (evt) { 
   var received_msg = evt.data;
   console.log('Message From Server: '+received_msg);
 }
 
+//When A Connection Is Established To The WebSocket
 ws.onopen = function(){
   console.log('Connected To WebSocket!');
 }
 
+//When The Connection Is Closed
 ws.onclose = function(){
   console.log('Disconnected From WebSocket!');
 }
