5d96302d722a979a1afd9673b0780df76e613221
Don <donvoowright@aol.com>
2016-02-02 09:08:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var ws = require("nodejs-websocket")
+/* Created By Donovan Wright
+This is the server code for node.js, hopefully it helps you get started!
+*/
+
+//Module Used
+var ws = require("nodejs-websocket");
 //This Will lisetn to port 8001
 var server = ws.createServer(function (conn) {
+	//When Someone Connects To The WebSocket
 	console.log("New connection")
 	conn.on("text", function (str) {
-		console.log("Received "+str)
-		conn.sendText(str.toUpperCase()+"!!!")
+		console.log("Received "+str); //The Message A Client Sends
+		conn.sendText(str.toUpperCase()+"!!!"); //Sending The Message Back In All Caps + !!!
 	})
+	//When A Client's Connection Closes From This WebSocket
 	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
+		console.log("Connection closed");
 	})
 }).listen(8001)
+// ^ Listens To The Port 8001 Ex: MY_IP_HERE:8001 <--
