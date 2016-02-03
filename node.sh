0ba8a14fedacd1938a503ee60f0c41d90944ed6e
Don <donvoowright@aol.com>
2016-02-03 13:09:36 -0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,11 @@
+sudo apt-get install curl -y
+sudo apt-get install npm -y
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install nodejs-websocket
+npm install readline
+npm install johnny-five
+cd
+cd ~/Desktop
+wget -O server.js "https://rawgit.com/donvoo/Node.js_School/master/server.js"
+wget -O client.js "https://rawgit.com/donvoo/Node.js_School/master/client.js"
